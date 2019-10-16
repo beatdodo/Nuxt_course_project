a2f4c983142e491a64c9f6709f6dc233584236d9
Archer <archer.huang@aceqilin.com>
2019-10-16 18:16:13 +0800

    Lecture 3 - Splitting the Page into Components

--- pages/index.vue
+++ pages/index.vue
@@ -4,101 +4,75 @@
             <h1>Get the latest tech news!</h1>
         </section>
         <section class="featured-posts">
-            <nuxt-link :to="'/posts/' + 1">
-                <article class="post-preview">
-                    <div class="post-thumbnail" style="background-image: url('https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg')"></div>
-                    <div class="post-content">
-                        <h1>Post Title</h1>
-                        <p>Preview Text</p>
-                    </div>
-                </article>
-            </nuxt-link>
-            <nuxt-link :to="'/posts/' + 2">
-                <article class="post-preview">
-                    <div class="post-thumbnail" style="background-image: url('https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1')"></div>
-                    <div class="post-content">
-                        <h1>Post Title 2</h1>
-                        <p>Preview Text 2</p>
-                    </div>
-                </article>
-            </nuxt-link>
+            <PostPreview
+                id="1"
+                thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
+                title="Hello there!"
+                previewText="This is my first post!"
+            />
+            <PostPreview
+                id="2"
+                thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
+                title="Hello there - the second time!"
+                previewText="This is my second post!"
+            />
+            <PostPreview
+                id="3"
+                thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
+                title="Hi!"
+                previewText="This is my third post!"
+            />
         </section>
     </div>
 </template>
 
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+
+export default {
+    components: {
+        PostPreview
+    }
+};
+</script>
+
 <style scoped>
 .intro {
-  height: 300px;
-  position: relative;
-  padding: 30px;
-  box-sizing: border-box;
-  background-position: center;
-  background-size: cover;
+    height: 300px;
+    position: relative;
+    padding: 30px;
+    box-sizing: border-box;
+    background-position: center;
+    background-size: cover;
 }
 
 .intro h1 {
-  position: absolute;
-  top: 10%;
-  left: 5%;
-  width: 90%;
-  font-size: 1.5rem;
-  color: black;
-  background-color: rgb(211, 211, 211);
-  padding: 10px;
-  border-radius: 10px;
-  box-shadow: 3px 3px 3px black;
-  box-sizing: border-box;
-  border: 1px solid black;
+    position: absolute;
+    top: 10%;
+    left: 5%;
+    width: 90%;
+    font-size: 1.5rem;
+    color: black;
+    background-color: rgb(211, 211, 211);
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 3px 3px 3px black;
+    box-sizing: border-box;
+    border: 1px solid black;
 }
 
 @media (min-width: 768px) {
-  .intro h1 {
-    font-size: 2rem;
-  }
+    .intro h1 {
+        font-size: 2rem;
+    }
 }
 
 .featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
 }
 </style>
