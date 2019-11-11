cbd771b20a8811b0cd148f813ff80ccc7061d04e
Archer <archer.huang@aceqilin.com>
2019-11-11 14:34:17 +0800

    Lecture 3 - Working on the Post Page

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+    <section class="post-list">
+        <PostPreview
+            id="1"
+            thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
+            title="Hello there!"
+            previewText="This is my first post!"
+        />
+        <PostPreview
+            id="2"
+            thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
+            title="Hello there - the second time!"
+            previewText="This is my second post!"
+        />
+        <PostPreview
+            id="3"
+            thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
+            title="Hi!"
+            previewText="This is my third post!"
+        />
+    </section>
+</template>
+
+<script>
+    import PostPreview from "@/components/Posts/PostPreview";
+
+    export default {
+        components: {
+            PostPreview
+        }
+    };
+</script>
+
+<style scoped>
+    .post-list {
+        display: flex;
+        padding: 20px;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,16 @@
         <section class="intro">
             <h1>Get the latest tech news!</h1>
         </section>
-        <section class="featured-posts">
-            <PostPreview
-                id="1"
-                thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
-                title="Hello there!"
-                previewText="This is my first post!"
-            />
-            <PostPreview
-                id="2"
-                thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
-                title="Hello there - the second time!"
-                previewText="This is my second post!"
-            />
-            <PostPreview
-                id="3"
-                thumbnail="https://st2.depositphotos.com/1297553/8277/v/600/depositphotos_82777500-stock-video-binary-digital-tech-data-code.jpg"
-                title="Hi!"
-                previewText="This is my third post!"
-            />
-        </section>
+        <PostList />
     </div>
 </template>
 
 <script>
-import PostPreview from "@/components/Posts/PostPreview";
+import PostList from "@/components/Posts/PostList";
 
 export default {
     components: {
-        PostPreview
+        PostList
     }
 };
 </script>
